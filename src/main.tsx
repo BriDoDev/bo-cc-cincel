e8c7ad38b8900d6933f57705a8dba0dadf6946a2
Briandv198 <briandv198@gmail.com>
2024-06-11 16:04:14 -0600

    login & forgot password :fire:

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import "./main.css";
+import "aos/dist/aos.css";
 
 // Fonts
 import "@fontsource/roboto/300.css";
@@ -9,9 +10,35 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+// Loader
+import Loader from "./Components/Loader/Loader.tsx";
+
+// Lazy loading for routes
+const Login = lazy(() => import("./Pages/Account/Login.tsx"));
+const ForgotPassword = lazy(() => import("./Pages/Account/ForgotPassword.tsx"));
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: (
+      <Suspense fallback={<Loader />}>
+        <Login />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/forgot-password",
+    element: (
+      <Suspense fallback={<Loader />}>
+        <ForgotPassword />
+      </Suspense>
+    ),
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
